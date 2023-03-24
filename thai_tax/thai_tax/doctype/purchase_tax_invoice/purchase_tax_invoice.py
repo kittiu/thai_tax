04e3e66a38bdc220472a886f2e157cb38a9ff195
Kitti U <kittiu@gmail.com>
2023-03-24 10:12:03 +0700

    Bug fix

--- thai_tax/thai_tax/doctype/purchase_tax_invoice/purchase_tax_invoice.py
+++ thai_tax/thai_tax/doctype/purchase_tax_invoice/purchase_tax_invoice.py
@@ -6,15 +6,20 @@ from frappe.utils import add_months
 
 class PurchaseTaxInvoice(Document):
 
+	def validate(self):
+		self.compute_report_date()
+
 	def on_update_after_submit(self):
 		if self.get_doc_before_save():  # Some change is made
-			# Compute Report Date
-			if int(self.months_delayed) == 0:
-				self.report_date = self.date
-			else:
-				self.report_date = add_months(self.date, int(self.months_delayed))
-			# Update Tax Invoice and Date back to original document, if changed
-			origin_doc = frappe.get_doc(self.voucher_type, self.voucher_no)
-			origin_doc.tax_invoice_number = self.number
-			origin_doc.tax_invoice_date = self.date
-			origin_doc.save(ignore_permissions=True)
+			self.compute_report_date()
+			if self.voucher_type and self.voucher_no:
+				origin_doc = frappe.get_doc(self.voucher_type, self.voucher_no)
+				origin_doc.tax_invoice_number = self.number
+				origin_doc.tax_invoice_date = self.date
+				origin_doc.save(ignore_permissions=True)
+
+	def compute_report_date(self):
+		if int(self.months_delayed) == 0:
+			self.report_date = self.date
+		else:
+			self.report_date = add_months(self.date, int(self.months_delayed))
--- thai_tax/thai_tax/doctype/sales_tax_invoice/sales_tax_invoice.py
+++ thai_tax/thai_tax/doctype/sales_tax_invoice/sales_tax_invoice.py
@@ -6,10 +6,15 @@ from frappe.utils import add_months
 
 class SalesTaxInvoice(Document):
 
+	def validate(self):
+		self.compute_report_date()
+
 	def on_update_after_submit(self):
 		if self.get_doc_before_save():  # Some change is made
-			# Compute Report Date
-			if int(self.months_delayed) == 0:
-				self.report_date = self.date
-			else:
-				self.report_date = add_months(self.date, int(self.months_delayed))
+			self.compute_report_date()
+
+	def compute_report_date(self):
+		if int(self.months_delayed) == 0:
+			self.report_date = self.date
+		else:
+			self.report_date = add_months(self.date, int(self.months_delayed))
