873d05131175f0234c763ca9801ad77d54ab12d4
Kitti U <kittiu@gmail.com>
2023-03-28 17:05:19 +0700

    Add Open Undue Tax Report

---
+++ thai_tax/custom/queries.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def undue_tax_query(doctype, txt, searchfield, start, page_len, filters):
+    setting = frappe.get_doc('Tax Invoice Settings')
+    res = list(set([setting.purchase_tax_account_undue, setting.sales_tax_account_undue]))
+    return [[x] for x in res]
